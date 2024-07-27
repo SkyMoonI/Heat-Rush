e448e47d4db118de204edb933846f4580e474802
Gokay Iseri <gokay.iseri@outlook.com>
2024-07-27 17:57:41 +0300

    player movement and camera added

--- Heat-Rush/Assets/Scripts/PlayerMovement.cs
+++ Heat-Rush/Assets/Scripts/PlayerMovement.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-	float VerticleInput;
-	float HorizontalInput;
+	[SerializeField] CharacterController characterController;
+	[SerializeField] Transform mainCamera;
 	float Speed = 5f;
-	// Start is called before the first frame update
-	void Start()
-	{
 
-	}
-
+	float turnSmoothTime = 0.1f;
+	float turnSmoothVelocity;
 	// Update is called once per frame
 	void Update()
 	{
+		// get keyboard input
+		float verticleInput = Input.GetAxis("Vertical");
+		float horizontalInput = Input.GetAxis("Horizontal");
+		// create movement vector
+		Vector3 direction = new Vector3(horizontalInput, 0, verticleInput).normalized;
 
-		VerticleInput = Input.GetAxis("Vertical");
-		HorizontalInput = Input.GetAxis("Horizontal");
-		transform.Translate(HorizontalInput * Speed * Time.deltaTime, 0, VerticleInput * Speed * Time.deltaTime);
+		// check if movement vector has a magnitude > 0.1f to move the player
+		if (direction.magnitude > 0.1f)
+		{
+			// get angle to rotate player. convert radian to degree because atan2 returns radian 
+			// and add camera angle to move the player relative to camera look direction
+			float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + mainCamera.eulerAngles.y;
+
+			// smooth turn angle
+			float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y,
+			targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+			// rotate player
+			transform.rotation = Quaternion.Euler(0, angle, 0);
+
+			// get movement direction relative to camera
+			Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+
+			// move player
+			characterController.Move(moveDirection * Speed * Time.deltaTime);
+		}
 	}
 }
